e79e4e7c2ea0a4caa7e8da49783ef025c6b307ad
rayan-ahamadi <115161695+rayan-ahamadi@users.noreply.github.com>
2024-03-13 20:50:04 +0100

    base de données

---
+++ commercial_db.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 13 mars 2024 à 08:07
+-- Version du serveur : 8.0.32
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `commercial_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etapes`
+--
+
+DROP TABLE IF EXISTS `etapes`;
+CREATE TABLE IF NOT EXISTS `etapes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nameEtape` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evenements`
+--
+
+DROP TABLE IF EXISTS `evenements`;
+CREATE TABLE IF NOT EXISTS `evenements` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idOpp` int NOT NULL,
+  `idEtape` int NOT NULL,
+  `actions` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idOpp` (`idOpp`),
+  KEY `idEtape` (`idEtape`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `opportunités`
+--
+
+DROP TABLE IF EXISTS `opportunités`;
+CREATE TABLE IF NOT EXISTS `opportunités` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) DEFAULT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `tel` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `idEtape` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idEtape` (`idEtape`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `evenements`
+--
+ALTER TABLE `evenements`
+  ADD CONSTRAINT `evenements_ibfk_1` FOREIGN KEY (`idOpp`) REFERENCES `opportunités` (`id`),
+  ADD CONSTRAINT `evenements_ibfk_2` FOREIGN KEY (`idEtape`) REFERENCES `opportunités` (`idEtape`);
+
+--
+-- Contraintes pour la table `opportunités`
+--
+ALTER TABLE `opportunités`
+  ADD CONSTRAINT `opportunités_ibfk_1` FOREIGN KEY (`idEtape`) REFERENCES `etapes` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
